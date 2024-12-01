bd2587aba054c35cda88a1b554a73ce2f973bdf3
Marius <elevmarius@list.ru>
2024-12-01 03:48:46 +0300

    Очистка репозитория от ненужных файлов

--- schemas.py
+++ schemas.py
@@ -4,22 +4,19 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-# Загрузка переменных окружения
 load_dotenv()
 
-# Переменные из .env файла
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
-# Настройка подключения к базе данных
+# подключение к бд
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
-# Определение модели таблицы Resume
 class Resume(Base):
     __tablename__ = "resumes"
 
@@ -34,8 +31,6 @@ class Resume(Base):
     social_links = Column(Text)
     salary = Column(String)
 
-# Создание таблиц
 Base.metadata.create_all(engine)
 
-# Создание сессии
 Session = sessionmaker(bind=engine)
